79c22245b6c377344db483e197184e0be5b2328b
Marnie Reid <marniejeanreid@gmail.com>
2020-05-03 11:54:17 -0600

    Complete Iteration 0

---
+++ ToDoListApp/ToDo.swift
@@ -0,0 +1,16 @@
+//
+//  ToDo.swift
+//  ToDoListApp
+//
+//  Created by Marnie Reid on 5/3/20.
+//  Copyright © 2020 Marnie Reid. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    
+    var name = ""
+    var important = false
+    
+}
